2339a1dbd1d2233bfd6d6f76d475af3b4d0fb4d2
mamogmx <marco.carbone@gmx.com>
2014-11-15 12:52:21 +0100

    Added search

--- iol/desktop/pg_desktop.py
+++ iol/desktop/pg_desktop.py
@@ -91,7 +91,7 @@ class pg_desktop(Container):
             return current_path[id]
         else:
             template_folder = getToolByName(self,'portal_skins')
-            template_folder = template_folder['pg_desktop_template']
+            template_folder = template_folder['pgdesktop_template']
             return  template_folder[id]
 
     def displayLayout(self):
