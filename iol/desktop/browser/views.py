7451a95c783e235c318bcd3af130a17478a3b3e8
mamogmx <marco.carbone@gmx.com>
2014-11-17 12:53:53 +0100

    commit

--- iol/desktop/browser/views.py
+++ iol/desktop/browser/views.py
@@ -4,7 +4,13 @@ from plone.dexterity.browser.view import DefaultView
 
 import simplejson as json
 
+
 class pgsearch(BrowserView):
+    def __init__(self, context, request):
+        # Each view instance receives context and request as construction parameters
+        self.context = context
+        self.request = request
+
     def __call__(self):
-        self.REQUEST.RESPONSE.headers['Content-Type'] = 'application/json'
+        self.request.RESPONSE.headers['Content-Type'] = 'application/json'
         return json.dumps(dict(a=1))
