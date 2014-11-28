39f4c3e25f02f0404581a065f7a3c8cd124d6c89
mamogmx <marco.carbone@gmx.com>
2014-11-28 09:44:48 +0100

    commit

--- iol/desktop/datatables.py
+++ iol/desktop/datatables.py
@@ -9,7 +9,7 @@ def inlist(key,v):
 def between(key,v):
     return "(coalesce(%s,'')<>'' AND ((%s)::%s >= '%s'::%s) AND ((%s)::%s <= '%s'::%s))" %(key,key,v['type'],v['value'][0],v['type'],key,v['type'],v['value'][1],v['type'])
 def contains(key,v):
-    return "((%s) ILIKE '%%s%')" %(key,v['value'][0])
+    return "((%s) ILIKE '%" + v['value'][0].replace("'","\'") +"%')" %(key)
 def equal(key,v):
     return "(coalesce(%s,'')<>'' AND (%s)::%s = '%s'::%s)" %(key,key,v["type"],v["value"][0],v["type"])
 
