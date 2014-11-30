fa39459ec2a61a20ab68839eb719eb4e816d95ad
mamogmx <marco.carbone@gmx.com>
2014-11-30 05:26:21 +0100

    Commit

--- iol/desktop/vocabularies.py
+++ iol/desktop/vocabularies.py
@@ -1,5 +1,5 @@
 from zope.schema.vocabulary import SimpleVocabulary
 
-map_position = SimpleVocabulary.fromItems([('nomap', 'No Map'), ('top', 'Position Top'),('bottom','Position Bottom')])
+map_position = SimpleVocabulary.fromItems([( 'No Map','nomap',), ( 'Position Top','top',),('Position Bottom','bottom',)])
 
 field_type = SimpleVocabulary.fromItems([('search_text', 'Text'), ('search_number', 'Number'),('search_date','Date'),('search_check','Check'),('search_list','List (Dynamic Search)'),])
