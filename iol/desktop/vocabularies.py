cb1b50e05ad1803b3bbb717ff5bedad275a6280b
mamogmx <marco.carbone@gmx.com>
2014-11-30 06:12:22 +0100

    Commit

--- iol/desktop/vocabularies.py
+++ iol/desktop/vocabularies.py
@@ -1,12 +1,14 @@
 from five import grok
+from zope.schema.interfaces import IContextSourceBinder
 from zope.schema.vocabulary import SimpleVocabulary
 from Products.CMFCore.utils import getToolByName
 
-class listGroups(object):
-    def __call__(self,context):
-        acl_users = getToolByName(self, 'acl_users')
-        group_list = acl_users.source_groups.getGroups()
-        return SimpleVocabulary.fromItems([(group.title, group.getName()) for group in group_list])
+@grok.provider(IContextSourceBinder)
+def listGroups(context):
+
+    acl_users = getToolByName(context, 'acl_users')
+    group_list = acl_users.source_groups.getGroups()
+    return SimpleVocabulary.fromItems([(group.title, group.getName()) for group in group_list])
 
 map_position = SimpleVocabulary.fromItems([( 'No Map','nomap',), ( 'Position Top','top',),('Position Bottom','bottom',)])
 
