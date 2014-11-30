1a47ebe23074b3c5cebf33ecf84cac962d3c7b6b
mamogmx <marco.carbone@gmx.com>
2014-11-30 05:57:56 +0100

    Commit

--- iol/desktop/vocabularies.py
+++ iol/desktop/vocabularies.py
@@ -1,5 +1,11 @@
 from zope.schema.vocabulary import SimpleVocabulary
+from plone import api
+
+def listGroups():
+    return SimpleVocabulary.fromItems([(grp.title,grp.groupname) for grp in api.group.get_groups()])
 
 map_position = SimpleVocabulary.fromItems([( 'No Map','nomap',), ( 'Position Top','top',),('Position Bottom','bottom',)])
 
-field_type = SimpleVocabulary.fromItems([('search_text', 'Text'), ('search_number', 'Number'),('search_date','Date'),('search_check','Check'),('search_list','List (Dynamic Search)'),])
+field_type = SimpleVocabulary.fromItems([( 'Text', 'search_text',), ('Number', 'search_number', ),('Date','search_date',),('Check','search_check',),('List (Dynamic Search)','search_list',),])
+
+users_groups_list = listGroups()
