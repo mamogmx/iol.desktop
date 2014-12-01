a89ef662f7549fe0e37f933fc8c59cff1ba8708b
mamogmx <marco.carbone@gmx.com>
2014-12-01 15:29:00 +0100

    commit

--- iol/desktop/datatables.py
+++ iol/desktop/datatables.py
@@ -17,7 +17,7 @@ def array_intersect(key,v):
     value = list()
     for vv in v['value']:
         value.append("'%s'::%s" %(vv,v['type']))
-    return "((%s)::%s && ARRAY[%s])" %(key,v['type'],",".join(value))
+    return "((%s)::_%s && ARRAY[%s])" %(key,v['type'],",".join(value))
 
 options = {
     'gt' : greaterthen,
