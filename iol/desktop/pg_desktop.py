82578cfd9ff7166419a786f85981f3ff7329e8ff
mamogmx <marco.carbone@gmx.com>
2014-12-13 06:13:48 +0100

    add Folder instead of CMFBTreeFolder

--- iol/desktop/pg_desktop.py
+++ iol/desktop/pg_desktop.py
@@ -42,8 +42,9 @@ class pg_desktop(Container):
     def __init__(self):
         Container.__init__(self)
         manage_addCMFBTreeFolder(self, id='resources')
+        self.invokeFactory('Folder', id='columns')
         #manage_addCMFBTreeFolder(self, id='columns')
-        api.content.create(container=self, type='Folder', id='columns')
+        #api.content.create(container=self, type='Folder', id='columns')
 
     def loadResources(self):
         result = dict(
